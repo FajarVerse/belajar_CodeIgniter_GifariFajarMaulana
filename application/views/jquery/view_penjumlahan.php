c920de7903aac6a5c55a6096f20b8b456ed9712e
Gifarfajar321 <gifarifajarm174@gmail.com>
2024-11-05 08:54:35 +0700

    matkul Ci 05/11/24

---
+++ application/views/jquery/view_penjumlahan.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h2>Form Penjumlahan</h2>
+  <div>
+    <input type="number" id="nilai1" placeholder="Nilai 1">
+  </div>
+  <div>
+    <input type="number" id="nilai2" placeholder="Nilai 2">
+  </div>
+  <div>
+    <button id="btnresult">Result</button>
+    <button id="btnreset">Reset</button>
+  </div>
+</body>
+
+</html>
